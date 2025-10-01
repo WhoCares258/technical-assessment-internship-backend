7b7a7782053e4586d449d4fa15478f577db99ac1
Umer Danka <22104053@imail.sunway.edu.my>
2025-10-01 10:22:24 +0800

    Added Fruit class
    Introduces a new Fruit class with a Name property, constructor, and ToString override for representing fruit objects by name.

--- TechnicalAssesmentBackendDeveloper/Program.cs
+++ TechnicalAssesmentBackendDeveloper/Program.cs
@@ -76,3 +76,17 @@ public class ItemManager<T>
         items = [];
     }
 }
+
+public class Fruit
+{
+    public string Name { get; set; }
+
+    public Fruit(string name)
+    {
+        Name = name;
+    }
+    public override string ToString()
+    {
+        return Name;
+    }
+}
